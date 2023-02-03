64745c8da7172b9cc78b79fcce08e19be0eaed72
astashev <slavik_82.09@mail.ru>
2023-02-03 12:09:04 +0300

    Init 1

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ru.astashev.ktor-auth-project"
---
+++ src/main/kotlin/ru/astashev/Application.kt
@@ -0,0 +1,15 @@
+package ru.astashev
+
+import io.ktor.server.application.*
+import ru.astashev.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
